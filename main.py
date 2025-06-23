e4e9e901085758e36557740d47e7b8441c7b2d0b
pavel <pavel@practice.ru>
2025-06-23 16:41:42 +0300

    new file:   json_test.txt 	modified:   main.py 	modified:   pyproject.toml 	new file:   templates/index.html 	new file:   templates/index2.html 	modified:   uv.lock

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from fastapi-test-1!")
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+from random import randint
 
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+templates = Jinja2Templates("./templates")
+
+@app.get("/")
+def root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+
+@app.get("/index2")
+def index2(request: Request):
+    return templates.TemplateResponse(request=request, name="index2.html")
+
+@app.get("/test_json")
+def test_json(reques: Request):
+    number = randint(0, 100000)
+    with open(file = "./json_test.txt", mode = "+a") as file:
+        file.write(f"{number}\n")
+    return number
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app=app, port=8555)
